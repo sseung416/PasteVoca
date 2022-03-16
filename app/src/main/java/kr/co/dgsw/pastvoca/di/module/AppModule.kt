ac852fd055b2b5cca31816e04f54cdfab720dbbe
최승연 <choiseungyeon416@gmail.com>
2022-03-16 09:03:05 +0900

    di koin setting

---
+++ app/src/main/java/kr/co/dgsw/pastvoca/di/module/AppModule.kt
@@ -0,0 +1,24 @@
+package kr.co.dgsw.pastvoca.di.module
+
+import kr.co.dgsw.pastvoca.repository.model.AppDatabase
+import kr.co.dgsw.pastvoca.repository.model.repository.VocabularyRepository
+import kr.co.dgsw.pastvoca.repository.model.repository.WordRepository
+import kr.co.dgsw.pastvoca.viewmodel.activity.MainViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val databaseModule = module {
+    single { AppDatabase.getInstance(androidContext()) }
+    single { get<AppDatabase>().vocabularyDao() }
+    single { get<AppDatabase>().wordDao() }
+}
+
+val repositoryModule = module {
+    single { VocabularyRepository(get()) }
+    single { WordRepository(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { MainViewModel(get(), get()) }
+}
