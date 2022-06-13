5494501da15c1d3188ca9a035b0d1f34ea7f2276
최승연 <choiseungyeon416@gmail.com>
2022-06-13 15:04:21 +0900

    STYLE BottomNavigation icon 수정

--- Android/app/src/main/java/co/kr/dgsw/searchvoca/view/activity/MainActivity.kt
+++ Android/app/src/main/java/co/kr/dgsw/searchvoca/view/activity/MainActivity.kt
@@ -16,16 +16,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setupToolbar()
+        setupBottomNavigation()
         changeFragment(HomeFragment())
-
-        findViewById<BottomNavigationView>(R.id.bottom_main).setOnItemSelectedListener {
-            return@setOnItemSelectedListener when (it.itemId) {
-                R.id.menu_home -> changeFragment(HomeFragment())
-                R.id.menu_test -> changeFragment(WordTestFragment())
-                R.id.menu_setting -> changeFragment(SettingFragment())
-                else -> false
-            }
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -36,6 +28,20 @@ class MainActivity : AppCompatActivity() {
     private fun setupToolbar() {
         setSupportActionBar(findViewById(R.id.toolbar2_main))
         supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun setupBottomNavigation() {
+        findViewById<BottomNavigationView>(R.id.bottom_main).apply {
+            itemIconTintList = null
+            setOnItemSelectedListener {
+                return@setOnItemSelectedListener when (it.itemId) {
+                    R.id.menu_home -> changeFragment(HomeFragment())
+                    R.id.menu_test -> changeFragment(WordTestFragment())
+                    R.id.menu_setting -> changeFragment(SettingFragment())
+                    else -> false
+                }
+            }
+        }
     }
 
     private fun changeFragment(fragment: Fragment): Boolean {
